a0a101aadc7946b7bb160d649990a2f9293fb177
CarlottaPolito <carlottapolitowd@gmail.com>
2025-06-04 18:06:28 +0200

    change format total price

--- js/app.js
+++ js/app.js
@@ -1,28 +1,26 @@
 const formPreventivo = document.getElementById('form-preventivo')
 const formJob = document.querySelector('.job-select')
 const inputCode = document.querySelector('.promo-code')
-const finalPrice= document.querySelector('#final-price')
 // console.log(finalPrice)
 
 formPreventivo.addEventListener('submit', function(e){
     e.preventDefault()
-    
-    const typeOfJob= formJob.value
-    const promoCode= inputCode.value.toUpperCase()
+
+
     const validCodes= ['YHDNU32', 'JANJC63','PWKCN25','SJDPO96','POCIE24']
     // console.log(validCodes)
     // console.log(typeOfJob)
     // console.log(promoCode)
 
-    let costPerHour = 0
+    let costPerHour = 0 
 
-    if(typeOfJob === 'back'){
+    if(formJob.value === 'back'){
         costPerHour = 20.50
     }
-    else if(typeOfJob === 'front'){
+    else if(formJob.value === 'front'){
         costPerHour = 15.30
     }
-    else if( typeOfJob === 'project'){
+    else if( formJob.value === 'project'){
         costPerHour = 33.60
     }
 
@@ -33,16 +31,19 @@ formPreventivo.addEventListener('submit', function(e){
     function isPromo (array){
         for ( let i= 0; i< array.length; i++){
             const isPromo = array[i]
-            if ( promoCode === isPromo){
+            if ( inputCode.value.toUpperCase() === isPromo){
                 return promo = (fullPrice*25)/100
             } 
         }
     }
     isPromo(validCodes)
     console.log(promo)
-    const userPrice= (fullPrice - promo).toFixed(2)
+    const userPrice= (fullPrice - promo)
 
     console.log(userPrice)
-    finalPrice.innerHTML= (userPrice)
+    document.querySelector('#final-price').innerHTML= new Intl.NumberFormat('it-IT', {
+		style: 'currency',
+		currency: 'EUR',
+	}).format(userPrice)
 
 })
