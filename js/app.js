3bd2fd86787ca0aba37fc6434322b68f1abf18af
CarlottaPolito <carlottapolitowd@gmail.com>
2025-06-06 16:14:35 +0200

    add alert

--- js/app.js
+++ js/app.js
@@ -1,49 +1,51 @@
 const formPreventivo = document.getElementById('form-preventivo')
 const formJob = document.querySelector('.job-select')
 const inputCode = document.querySelector('.promo-code')
-// console.log(finalPrice)
-
-formPreventivo.addEventListener('submit', function(e){
-    e.preventDefault()
 
 
-    const validCodes= ['YHDNU32', 'JANJC63','PWKCN25','SJDPO96','POCIE24']
-    // console.log(validCodes)
-    // console.log(typeOfJob)
-    // console.log(promoCode)
-
-    let costPerHour = 0 
-
-    if(formJob.value === 'back'){
-        costPerHour = 20.50
+let havePromo = false
+function isPromo (array){
+    for ( let i= 0; i < array.length; i++){
+        if ( inputCode.value.toUpperCase() === array[i]){
+            return havePromo=true
+        } 
     }
-    else if(formJob.value === 'front'){
-        costPerHour = 15.30
-    }
-    else if( formJob.value === 'project'){
-        costPerHour = 33.60
-    }
+}
 
-    const fullPrice = 10 * costPerHour
-    // console.log(costPerHour, fullPrice)
-    
-    let promo = 0
-    function isPromo (array){
-        for ( let i= 0; i< array.length; i++){
-            const isPromo = array[i]
-            if ( inputCode.value.toUpperCase() === isPromo){
-                return promo = (fullPrice*25)/100
-            } 
+    formPreventivo.addEventListener('submit', function(e){
+        e.preventDefault()
+
+
+        const validCodes= ['YHDNU32', 'JANJC63','PWKCN25','SJDPO96','POCIE24']
+        isPromo(validCodes)
+       if (havePromo === false && inputCode.value !== ''){
+        alert(' Il codice inserito non è valido, perciò il prezzo finale verrà calcolato senza applicare sconti')
+       }
+
+        let costPerHour = 0 
+
+        if(formJob.value === 'back'){
+            costPerHour = 20.50
         }
-    }
-    isPromo(validCodes)
-    console.log(promo)
-    const userPrice= (fullPrice - promo)
+        else if(formJob.value === 'front'){
+            costPerHour = 15.30
+        }
+        else if( formJob.value === 'project'){
+            costPerHour = 33.60
+        }
 
-    console.log(userPrice)
-    document.querySelector('#final-price').innerHTML= new Intl.NumberFormat('it-IT', {
-		style: 'currency',
-		currency: 'EUR',
-	}).format(userPrice)
+        const fullPrice = 10 * costPerHour
+        let promo = 0
+        if (havePromo === true){
+            promo= (fullPrice * 25)/100
+        }
+        
+        const userPrice= (fullPrice - promo)
+
+        console.log(havePromo,costPerHour, fullPrice, promo, userPrice)
+        // document.querySelector('#final-price').innerHTML= new Intl.NumberFormat('it-IT', {
+        //     style: 'currency',
+        //     currency: 'EUR',
+        // }).format(userPrice)
 
 })
